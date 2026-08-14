1e34a87c88a7a73c75fa7015bf98861bcaa4a4a7
Lukas LukyLuke_ch Zurschmiede <lukas.zurschmiede@ranta.ch>
2026-08-14 16:10:08 +0200

    include projects and console_debug macro in webui

--- confui/src/lib.rs
+++ confui/src/lib.rs
@@ -1,12 +1,34 @@
 mod utils;
 
+use shared::types::Queue;
 use wasm_bindgen::prelude::*;
+
+use recv_polling;
+
 
 /// Macro to simplify calls to console.log("", ...) in the browser
 /// Use it like `console_log!("Format String", args...)`
+#[allow(unused_macros)]
 macro_rules! console_log {
 	($($args:tt)*) => {
 		web_sys::console::log_1(&format_args!($($args)*).to_string().into());
+	};
+}
+#[allow(unused_macros)]
+macro_rules! console_error {
+	($($args:tt)*) => {
+		web_sys::console::error_1(&format_args!($($args)*).into());
+	};
+}
+#[allow(unused_macros)]
+macro_rules! console_debug {
+	($value:expr) => {{
+		let js_val = serde_wasm_bindgen::to_value(&$value).unwrap_or_default();
+		web_sys::console::debug_1(&js_val);
+	}};
+	($($args:tt)*) => {
+		let js_val = serde_wasm_bindgen::to_value(&format!($($args)*)).unwrap_or_default();
+		web_sys::console::debug_1(&js_val);
 	};
 }
 
@@ -18,5 +40,16 @@ extern "C" {
 
 #[wasm_bindgen]
 pub fn greet(name: &str) {
+	let xx = recv_polling::types::Config{
+		config: recv_polling::types::Polling{
+			name: "Name blubber".to_string(),
+			api: vec![],
+			timer: "".to_string(),
+			queue: Queue::default(),
+			parser: vec![],
+		},
+	};
 	console_log!("Hello {} - how is your {}", name, "day");
+
+	console_debug!(xx);
 }
